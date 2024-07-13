8abb21cb0df3488aec340fa401b47c0adeca3a04
YahyaMohand <96312354+YahyaMohand@users.noreply.github.com>
2024-07-13 05:41:36 +0300

    update the structure of code

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "test-terraform-state-bucket-123123"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "test-terraform-lock-table"
-  }
-}
